8412bc4f6dce2ceed91e8dd4ad1ffd374338d266
Manny Cruz <79336032+cruzmanny@users.noreply.github.com>
2023-06-14 22:16:24 -0400

    Update main class to check for null or empty input

--- src/palindrome/Main.java
+++ src/palindrome/Main.java
@@ -5,6 +5,11 @@ public class Main {
         System.out.print("Please Enter a Word, Sentence, or Phrase: ");
         Scanner input = new Scanner(System.in);
         String sentence = input.nextLine();
+        while(sentence.isBlank()){
+            System.out.println("Nothing Was Entered. Please Try Again.");
+            System.out.print("Please Enter a Word, Sentence, or Phrase: ");
+            sentence = input.nextLine();
+        }
         System.out.printf("%s%n","You Have Entered: " + sentence);
         System.out.print("Your Entry Reversed: ");
         String reversed = String.valueOf(Palindrome.reverse(sentence));
