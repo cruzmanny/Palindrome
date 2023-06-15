41683188c7d96a68dbf1215377839bac655b71a0
Manny Cruz <79336032+cruzmanny@users.noreply.github.com>
2023-06-14 22:15:39 -0400

    Create main class to execute program

--- src/palindrome/Main.java
+++ src/palindrome/Main.java
@@ -1,7 +1,15 @@
 package palindrome;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+        System.out.print("Please Enter a Word, Sentence, or Phrase: ");
+        Scanner input = new Scanner(System.in);
+        String sentence = input.nextLine();
+        System.out.printf("%s%n","You Have Entered: " + sentence);
+        System.out.print("Your Entry Reversed: ");
+        String reversed = String.valueOf(Palindrome.reverse(sentence));
+        System.out.printf("%s%n",reversed);
+        Palindrome.isPalindrome(sentence, reversed);
 
     }
 }
