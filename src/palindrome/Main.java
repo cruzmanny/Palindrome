dfb1fc4e63512c195c6157bf3dd08ad1d31f70c2
Manny Cruz <79336032+cruzmanny@users.noreply.github.com>
2023-06-14 22:39:19 -0400

    Add javadoc and regex bug fix

--- src/palindrome/Main.java
+++ src/palindrome/Main.java
@@ -1,3 +1,11 @@
+/**
+ * @author Manny Cruz
+ * @since 14-06-2023
+ * The Palindrome program receives input from a user, reverses the order,
+ * and then compares the strings to check if it is a palindrome.
+ * The program will accept words, numbers, phrases and sentences with white spaces, or hyphenated words.
+ * It will not accept null, blank, or an empty string.
+ */
 package palindrome;
 import java.util.Scanner;
 public class Main {
@@ -15,6 +23,5 @@ public class Main {
         String reversed = String.valueOf(Palindrome.reverse(sentence));
         System.out.printf("%s%n",reversed);
         Palindrome.isPalindrome(sentence, reversed);
-
     }
 }
--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -1,15 +1,37 @@
+/**
+ * @author Manny Cruz
+ * @since 14-06-2023
+ * The Palindrome program receives input from a user, reverses the order,
+ * and then compares the strings to check if it is a palindrome.
+ * The program will accept words, numbers, phrases and sentences with white spaces, or hyphenated words.
+ * It will not accept null, blank, or an empty string.
+ */
 package palindrome;
-
+import java.util.regex.*;
 public class Palindrome {
+    /**
+     * Returns the word, sentence, or phrase entered in reversed order.
+     * Entry can not be null.
+     * @param userInput the word, sentence or phrase entered
+     * @return The user's input in reversed order
+     */
     public static StringBuilder reverse(String userInput){
         StringBuilder reversedString = new StringBuilder(String.valueOf(userInput));
         reversedString.reverse();
         return reversedString;
     }
+    /**\
+     * Checks if the entry is a palindrome by comparing each character of the string entered with the string in reverse.
+     * Removes any white spaces and hyphens (will accept hyphenated words and phrases or sentences with spaces)
+     * before the comparison. If a mismatch is detected it will output the string is not a palindrome else,
+     * the output will show a success message.
+     * @param userEntry the entry string made by the user
+     * @param reversedEntry the entry string in reverse order
+     */
     public static void isPalindrome(String userEntry, String reversedEntry){
         boolean b = true;
-        userEntry = userEntry.replaceAll("\\s | \\-", "");
-        reversedEntry = reversedEntry.replaceAll("\\s | \\-", "");
+        userEntry = userEntry.replaceAll("[\\s\\-]", "");
+        reversedEntry = reversedEntry.replaceAll("[\\s\\-]", "");
         for (int i = 0; i < userEntry.length(); i++){
             if (userEntry.charAt(i) == reversedEntry.charAt(i)) {
             } else{
