b8999b8378be4a55024c53ccf97554e5cfcf5038
Manny Cruz <79336032+cruzmanny@users.noreply.github.com>
2023-06-16 16:46:30 -0400

    Refactor Main & Palindrome classes

--- src/palindrome/Main.java
+++ src/palindrome/Main.java
@@ -1,27 +1,25 @@
 /**
  * @author Manny Cruz
  * @since 14-06-2023
- * The Palindrome program receives input from a user, reverses the order,
- * and then compares the strings to check if it is a palindrome.
- * The program will accept words, numbers, phrases and sentences with white spaces, or hyphenated words.
+ * @version 1.0.1
+ * The Palindrome program receives input from a user, reverses the order, and then compares the strings to check if it is a palindrome.
+ * The program will accept words, numbers, phrases and sentences with white spaces, and commonly used special characters.
  * It will not accept null, blank, or an empty string.
  */
 package palindrome;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.print("Please Enter a Word, Sentence, or Phrase: ");
+        System.out.print("Please enter a word, sentence, or phrase: ");
         Scanner input = new Scanner(System.in);
         String sentence = input.nextLine();
         while(sentence.isBlank()){
-            System.out.println("Nothing Was Entered. Please Try Again.");
-            System.out.print("Please Enter a Word, Sentence, or Phrase: ");
+            System.out.println("Nothing was entered. Please try again.");
+            System.out.print("Please enter a word, sentence, or phrase: ");
             sentence = input.nextLine();
         }
-        System.out.printf("%s%n","You Have Entered: " + sentence);
-        System.out.print("Your Entry Reversed: ");
+        input.close();
         String reversed = String.valueOf(Palindrome.reverse(sentence));
-        System.out.printf("%s%n",reversed);
         Palindrome.isPalindrome(sentence, reversed);
     }
 }
--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -1,17 +1,15 @@
 /**
  * @author Manny Cruz
  * @since 14-06-2023
- * The Palindrome program receives input from a user, reverses the order,
- * and then compares the strings to check if it is a palindrome.
- * The program will accept words, numbers, phrases and sentences with white spaces, or hyphenated words.
+ * @version 1.0.1
+ * The Palindrome program receives input from a user, reverses the order, and then compares the strings to check if it is a palindrome.
+ * The program will accept words, numbers, phrases and sentences with white spaces, and commonly used special characters.
  * It will not accept null, blank, or an empty string.
  */
 package palindrome;
-import java.util.regex.*;
 public class Palindrome {
     /**
-     * Returns the word, sentence, or phrase entered in reversed order.
-     * Entry can not be null.
+     * Returns the word, sentence, or phrase entered in reversed order. Entries can not be null.
      * @param userInput the word, sentence or phrase entered
      * @return The user's input in reversed order
      */
@@ -21,28 +19,22 @@ public class Palindrome {
         return reversedString;
     }
     /**\
-     * Checks if the entry is a palindrome by comparing each character of the string entered with the string in reverse.
-     * Removes any white spaces and hyphens (will accept hyphenated words and phrases or sentences with spaces)
-     * before the comparison. If a mismatch is detected it will output the string is not a palindrome else,
-     * the output will show a success message.
+     * Checks if the entry is a palindrome by comparing the string entered with the string in reverse.
+     * Removes any white spaces and commonly used special characters before the comparison.
+     * If a match is detected it will output a success message else, a mismatch message.
      * @param userEntry the entry string made by the user
      * @param reversedEntry the entry string in reverse order
      */
     public static void isPalindrome(String userEntry, String reversedEntry){
-        boolean b = true;
-        userEntry = userEntry.replaceAll("[\\s\\-]", "");
-        reversedEntry = reversedEntry.replaceAll("[\\s\\-]", "");
-        for (int i = 0; i < userEntry.length(); i++){
-            if (userEntry.charAt(i) == reversedEntry.charAt(i)) {
-            } else{
-                b = false;
-                break;
-            }
+        String regex = "[\\s\"',;.:?!-]";
+        userEntry = userEntry.replaceAll(regex, "");
+        reversedEntry = reversedEntry.replaceAll(regex, "");
+        String output = "You have entered: " + userEntry + "\nYour entry reversed: " + reversedEntry;
+        if (userEntry.equals(reversedEntry)){
+            System.out.print(output + "\nYou have entered a palindrome :)");
         }
-        if (!b) {
-            System.out.println("You did not enter a Palindrome :(");
-        } else {
-            System.out.println("You have entered a Palindrome :)");
+        else {
+            System.out.print(output + "\nYou did not enter a palindrome :(");
         }
     }
 }
