c56883df7dc0987b87c781568f2a18f44316dc45
Manny Cruz <79336032+cruzmanny@users.noreply.github.com>
2023-06-18 15:13:58 -0400

    Add github action

--- src/test/java/com/palindrome/PalindromeTests.java
+++ src/test/java/com/palindrome/PalindromeTests.java
@@ -2,7 +2,8 @@ package com.palindrome;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-class PalindromeTest {
+
+class PalindromeTests {
     @Test
     @DisplayName("Reverse String")
     void reverse() {
